d60e34f989a80ba24d13ef55dab6f0da80239326
code555man <dekdee1887@gmail.com>
2023-11-20 13:15:11 +0700

    add mul.html

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-// console.log('hello');
+const { createApp, ref } = Vue;
+
+createApp({
+    data() {
+        return {
+            number: '',
+            lt: [],
+            t: '',
+        }
+    },
+    methods: {
+        mul(){
+            let n = this.number;
+
+            for(let i =1; i<=12; i++){
+                this.t += `${i} x ${n} = ${n*i}`;
+            }
+            
+        }
+    }
+}).mount('#app')
